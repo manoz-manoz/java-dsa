3bd3b80bc8df61338323994004603f090d70e146
Manoz <gummamanoz@gmail.com>
2026-05-16 10:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (41.79%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] arr, int val) {
+        int index=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=val)
+                arr[index++]=arr[i];
+        }
+        return index;
+        
+    }
+}
